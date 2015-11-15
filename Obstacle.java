dfad4382e7d16435e3f10ace7d5d41d6a5e06a46
Alice Chang <avc2120@columbia.edu>
2015-11-15 02:25:10 -0500

    reads files

---
+++ Obstacle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Obstacle
+{
+	private ArrayList<Point> vertices;
+	public Obstacle()
+	{
+		vertices = new vertices();
+	}
+
+	public void addVertex(Point p)
+	{
+		vertices.add(p);
+	}
+}
--- hwk4_team11.java
+++ hwk4_team11.java
@@ -3,12 +3,13 @@ public class Hwk4_team11
 {
 	private Point start;
 	private Point goal;
+	private static ArrayList<Obstacle> obstacles = new ArrayList<Obstacle>();
 	public static void main(String[] args)
 	{
 
 	}
 
-	public void readStartGoal(String filename)
+	public void readStartGoal(String fileName)
 	{
 		try {
             FileReader fileReader = new FileReader(fileName);
@@ -28,15 +29,32 @@ public class Hwk4_team11
             bufferedReader.close();         
         }
         catch(FileNotFoundException ex) {
-            System.out.println(
-                "Unable to open file '" + 
-                fileName + "'");                
+            System.out.println("Unable to open file '" + fileName + "'");                
         }
 
 	}
 
-	public void readObstacles(String filename)
+	public void readObstacles(String fileName)
 	{
-
+		try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            Scanner in = new Scanner(bufferedReader);
+			int num_obstacles = in.nextInt();
+			for(int i = 0; i < num_obstacles; i++)
+			{
+				int num_vertices = in.nextInt();
+				Obstacle obstacle = new obstacle();
+				for(int j = 0; j < num_vertices; j++)
+				{
+					double[] points = in.nextLine().split(" ");
+					obstacle.addVertex(new Point(points[0], points[1]));
+				}
+				obstacles.add(obstacle);
+			}         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");                
+        }
 	}
 }
